2352038e37cdfdeabf5b7091a2613b8a68e4a124
takecare <rui@vazteixeira.org>
2021-10-03 21:22:35 +0100

    final attempt at global translate approach

--- Node2D.gd
+++ Node2D.gd
@@ -4,6 +4,7 @@ onready var sprite = $Sprite
 onready var center = $Sprite2
 
 var offset = 150
+var speed = 20
 
 func _ready() -> void:
   sprite.position = Vector2(center.position.x + offset, center.position.y + offset)
@@ -13,26 +14,27 @@ func rot(obj: Node2D, point: Node2D, angle):
   obj.global_transform = point.global_transform * Transform2D().rotated(deg2rad(angle))
 
 func rotateAround(obj: Node2D, point, angle):
-    var rot = deg2rad(angle)
-    obj.global_translate(-point)
-    obj.transform = obj.transform.rotated(rot)
-    obj.global_translate(point)
-#    sprite.look_at(center.position)
+  var rot = deg2rad(angle)
+  var x = point.x
+  var y = point.y
+  obj.global_translate(-Vector2(x,y))
+  obj.transform = obj.transform.rotated(rot)
+  obj.global_translate(Vector2(x,y))
+  sprite.look_at(center.position)
 
 func rot2(obj, point, angle) -> void:
   var rot = deg2rad(angle)
   var x = point.x
   var y = point.y
   var trnsf = Transform2D()
-  #trnsf.origin = Vector2(200, 300)
-  trnsf.origin.x = -x * cos(angle) + y * sin(angle) + x
-  trnsf.origin.y = -x * sin(angle) + y * cos(angle) - y
-#  obj.global_translate(-point)
+  obj.global_translate(-point)
   trnsf.x = Vector2(cos(rot), sin(rot))
   trnsf.y = Vector2(-sin(rot), cos(rot))
   obj.transform = trnsf
   obj.global_translate(point)
 
+var angle = 0
+var step = 5
 func setpos(obj, point, angle) -> void:
   var a = deg2rad(angle)
   var x = point.x + offset * cos(a)
@@ -41,14 +43,15 @@ func setpos(obj, point, angle) -> void:
   obj.global_position.y = y
   pass
 
-var angle = 0
-
 func _process(delta):
-#  rotateAround(sprite, center.position, -25 * delta)
-  if angle >= 360.0:
-    angle = 0
-  angle =  angle + -25 * delta
-  setpos(sprite, center.position, angle)
+#  if angle == 360:
+#    angle = 0
+#  elif angle > 360:
+#    angle = abs(360 - angle)
+#  angle += step * delta * speed
+#  setpos(sprite, center.position, angle)
+
+  rotateAround(sprite, center.position, -25 * delta)
 #  rot2(sprite, center.position, -25 * delta)
 #  rot(sprite, center, -25 * delta)
   pass
