76334c2c5668ce152d2996d31f8282dbaaee36bb
takecare <rui@vazteixeira.org>
2021-10-12 23:43:23 +0100

    add digit sprites

--- scripts/Score.gd
+++ scripts/Score.gd
@@ -1,9 +1,23 @@
-class_name Score extends Label
+class_name Score extends TextureRect
 
 export(int) var score = 0
 
+const sprites = [
+  preload("res://sprites/0.png"),
+  preload("res://sprites/1.png"),
+  preload("res://sprites/2.png"),
+  preload("res://sprites/3.png"),
+  preload("res://sprites/4.png"),
+  preload("res://sprites/5.png"),
+  preload("res://sprites/6.png"),
+  preload("res://sprites/7.png"),
+  preload("res://sprites/8.png"),
+  preload("res://sprites/9.png")
+]
+
 func _ready() -> void:
-  text = str(score)
+  #text = str(score)
+  pass
 
 func center_on(point: Vector2):
   set_global_position(
@@ -13,9 +27,13 @@ func center_on(point: Vector2):
 func increase():
   # TODO animate (quick scale+fade)
   score += 1
-  text = str(score)
 
 func reset():
   # TODO animate counting down until 0
   score = 0
-  text = str(score)
+
+func update():
+  var readable = str(score)
+  for i in range(0, readable.length()):
+    print(i)
+  pass
