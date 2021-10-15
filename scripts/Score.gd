4b6925eee68e44a3838a0567d852094fc1bba823
takecare <rui@vazteixeira.org>
2021-10-15 08:58:08 +0100

    wip: add score layer animations still facing an issue where the pivot point of the hbox is not updated when one of its children goes invisible

--- scripts/Score.gd
+++ scripts/Score.gd
@@ -3,7 +3,6 @@ class_name Score extends Node2D
 export(int) var score = 0
 
 onready var currentScoreLayer: ScoreLayer = $CurrentScoreLayer
-onready var animationPlayer: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
   #text = str(score)
@@ -22,12 +21,15 @@ func increase():
 func reset():
   # TODO animate counting down until 0
   score = 0
+  currentScoreLayer.reset()
   pass
 
 func _unhandled_key_input(event: InputEventKey) -> void:
   if event.echo || event.pressed: return
   if event.scancode == KEY_G:
-    animationPlayer.play("Increase")
+    currentScoreLayer.disappear()
+  elif event.scancode == KEY_H:
+    currentScoreLayer.reset()
   elif event.scancode == KEY_F:
     currentScoreLayer.increase()
   get_tree().set_input_as_handled()
--- scripts/ScoreLayer.gd
+++ scripts/ScoreLayer.gd
@@ -2,11 +2,13 @@ class_name ScoreLayer extends CenterContainer
 
 export(int) var score = 0 setget set_score,get_score
 
+onready var animationPlayer: AnimationPlayer = $AnimationPlayer
 onready var hBoxContainer: HBoxContainer = $HBoxContainer
-onready var digit1: TextureRect = $HBoxContainer/Digit1
-onready var digit2: TextureRect = $HBoxContainer/Digit2
-onready var digit3: TextureRect = $HBoxContainer/Digit3
+#onready var digit1: TextureRect = $HBoxContainer/Digit1
+#onready var digit2: TextureRect = $HBoxContainer/Digit2
+#onready var digit3: TextureRect = $HBoxContainer/Digit3
 
+# we'll have 2 ScoreLayers - does the engine share these sprites or does it load them twice?
 const sprites = [
   preload("res://sprites/0.png"),
   preload("res://sprites/1.png"),
@@ -21,26 +23,12 @@ const sprites = [
 ]
 
 func _ready() -> void:
-  #text = str(score)
   offset_pivot()
-  pass
 
 func center_on(point: Vector2):
   set_global_position(
     Vector2(point.x - rect_size.x / 2, point.y - rect_size.y / 2)
   )
-
-func increase():
-  # TODO animate (quick scale+fade)
-  score += 1
-  update()
-  offset_pivot() # TODO re-calculate pivot offset on hBoxContainer
-
-func reset():
-  # TODO animate counting down until 0
-  score = 0
-  update()
-  offset_pivot() # TODO re-calculate pivot offset on hBoxContainer
 
 func get_score() -> int:
   return score
@@ -50,28 +38,36 @@ func set_score(new: int) -> void:
   update()
   offset_pivot() # TODO re-calculate pivot offset on hBoxContainer
 
+func disappear() -> void:
+  animationPlayer.play("Disappear")
+
+func reset() -> void:
+  animationPlayer.play_backwards("Disappear")
+
 func offset_pivot():
   # pivot offset on hBoxContainer must be its w/2 and its h/2
-  hBoxContainer.rect_pivot_offset = hBoxContainer.rect_size / 2
+  $HBoxContainer.rect_pivot_offset = $HBoxContainer.rect_size / 2
 
+# we have to reference nodes directly as this is called in the score setter,
+# which means this runs before _ready()/onready
 func update():
   var readable = str(score)
   var length = readable.length()
   if length == 1:
-    digit1.visible = true
-    digit2.visible = false
-    digit3.visible = false
-    digit1.texture = sprites[readable[0].to_int()]
+    $HBoxContainer/Digit1.texture = sprites[readable[0].to_int()]
+    $HBoxContainer/Digit1.visible = true
+    $HBoxContainer/Digit2.visible = false
+    $HBoxContainer/Digit3.visible = false
   elif length == 2:
-    digit1.visible = true
-    digit2.visible = true
-    digit3.visible = false
-    digit1.texture = sprites[readable[0].to_int()]
-    digit2.texture = sprites[readable[1].to_int()]
+    $HBoxContainer/Digit1.texture = sprites[readable[0].to_int()]
+    $HBoxContainer/Digit2.texture = sprites[readable[1].to_int()]
+    $HBoxContainer/Digit1.visible = true
+    $HBoxContainer/Digit2.visible = true
+    $HBoxContainer/Digit3.visible = false
   else:
-    digit1.visible = true
-    digit2.visible = true
-    digit3.visible = true
-    digit1.texture = sprites[readable[0].to_int()]
-    digit2.texture = sprites[readable[1].to_int()]
-    digit3.texture = sprites[readable[2].to_int()]
+    $HBoxContainer/Digit1.texture = sprites[readable[0].to_int()]
+    $HBoxContainer/Digit2.texture = sprites[readable[1].to_int()]
+    $HBoxContainer/Digit3.texture = sprites[readable[2].to_int()]
+    $HBoxContainer/Digit1.visible = true
+    $HBoxContainer/Digit2.visible = true
+    $HBoxContainer/Digit3.visible = true
