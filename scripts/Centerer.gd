15724de96db0c3848fb50fe5bbc9951f1b405ce6
takecare <rui@vazteixeira.org>
2021-10-20 09:01:16 +0100

    wip: pack children + only look at control nodes

--- scripts/Centerer.gd
+++ scripts/Centerer.gd
@@ -2,39 +2,56 @@ extends Container
 
 # goal: update pivot_offest when children are hidden/displayed
 
-# need to know total available width - i.e. my own width
-# need to know space (horizontal) between each item to place
-#
+enum LayoutMode { SPREAD, PACKED }
+var mode = LayoutMode.PACKED
+
+var controls: Array
 
 func _ready() -> void:
-  pass
+  for i in get_child_count():
+    if get_child(i).is_class("Control"):
+      controls.push_back(get_child(i))
 
 func _process(_delta: float) -> void:
   pass
 
-enum LayoutMode { SPREAD, PACKED }
-var mode = LayoutMode.PACKED
 
 func _notification(what):
   if what == NOTIFICATION_SORT_CHILDREN:
     assert(mode == LayoutMode.SPREAD || mode == LayoutMode.PACKED, "Unsupported layout mode.")
-    print("> re-size requested")
     if mode == LayoutMode.PACKED:
       layout_children_packed()
     elif mode == LayoutMode.SPREAD:
       layout_children_spread()
 
+
 func layout_children_packed() -> void:
-  for i in get_child_count():
-      var child = get_child(i)
-      if child.is_class("Control"):
-        layout_child_packed(i, child)
+  print("> PACKED")
+  var num = controls.size()
+  var rect_size = _get_rect_size()
+  var available_width = rect_size.x
+  var mid_point_x = available_width / 2
+  print("> av. width = " + str(available_width))
+  print("> mid pt x = " + str(mid_point_x))
+  print("> width unit = " + str(controls[0].rect_size.x))
+
+  if num % 2 == 0: # even num of children
+    var starting_x = mid_point_x - (num / 2)
+    for i in num:
+      layout_child_packed(starting_x, i, controls[i])
+  else: # odd num of children
+    var width_unit = controls[0].rect_size.x
+    var starting_x = mid_point_x - floor(num / 2) * width_unit
+    for i in num:
+      layout_child_packed(starting_x, i, controls[i])
+
 
 func layout_children_spread() -> void:
   for i in get_child_count():
       var child = get_child(i)
       if child.is_class("Control"):
         layout_child_spread(i, child)
+
 
 # spread children horizontally
 func layout_child_spread(num: int, child: Control) -> void:
@@ -47,15 +64,20 @@ func layout_child_spread(num: int, child: Control) -> void:
   var child_size = Vector2(width_per_child, 0)
   fit_child_in_rect(child, Rect2(child_pos, child_size))
 
+
 #
-func layout_child_packed(num: int, child: Control) -> void:
+func layout_child_packed(starting_x: int, i: int, child: Control) -> void:
   var rect_size = _get_rect_size()
   var available_width = rect_size.x
   var available_height = rect_size.y
   var center = rect_size / 2
-  # middle child must be centered within this container, if we have an odd number of children
-  # if the num of children are even, then we have to place X/2 to the left and X/2 to the right of the center
-  pass
+  var child_size = Vector2(get_child(0).rect_size.x, 0)
+  var child_x_offset = child_size.x / 2
+  var child_pos = Vector2(starting_x * (i + 1) - child_x_offset, 0)
+
+  print("x["+str(i)+"]="+str(child_pos.x))
+
+  fit_child_in_rect(child, Rect2(child_pos, child_size))
 
 
 func _get_rect_size() -> Vector2:
@@ -64,6 +86,12 @@ func _get_rect_size() -> Vector2:
   else:
     return rect_size
 
+func _get_child_count() -> int:
+  var count = 0
+  for i in get_child_count():
+    if  get_child(i).is_class("Control"):
+      count += 1
+  return count
 
 func set_some_setting():
   # Some setting changed, ask for children re-sort
