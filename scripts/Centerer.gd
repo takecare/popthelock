93ab627363848cf99002a02a605285e2cf331bbf
Rui Teixeira <rui@vazteixeira.org>
2021-10-31 17:12:12 +0000

    center packed

--- scripts/Centerer.gd
+++ scripts/Centerer.gd
@@ -3,7 +3,7 @@ extends Container
 # goal: update pivot_offest when children are hidden/displayed
 
 enum LayoutMode { SPREAD, PACKED }
-var mode = LayoutMode.PACKED
+export(LayoutMode) var layout_mode = LayoutMode.PACKED
 
 var controls: Array
 
@@ -18,16 +18,15 @@ func _process(_delta: float) -> void:
 
 func _notification(what):
   if what == NOTIFICATION_SORT_CHILDREN:
-    assert(mode == LayoutMode.SPREAD || mode == LayoutMode.PACKED, "Unsupported layout mode.")
-    if mode == LayoutMode.PACKED:
+    assert(layout_mode == LayoutMode.SPREAD || layout_mode == LayoutMode.PACKED, "Unsupported layout mode.")
+    if layout_mode == LayoutMode.PACKED:
       layout_children_packed()
-    elif mode == LayoutMode.SPREAD:
+    elif layout_mode == LayoutMode.SPREAD:
       layout_children_spread()
 
 
 func layout_children_packed() -> void:
-  print("> PACKED")
-  var num = controls.size()
+  var num_children = controls.size()
   var rect_size = _get_rect_size()
   var available_width = rect_size.x
   var mid_point_x = available_width / 2
@@ -35,22 +34,24 @@ func layout_children_packed() -> void:
   print("> mid pt x = " + str(mid_point_x))
   print("> width unit = " + str(controls[0].rect_size.x))
 
-  if num % 2 == 0: # even num of children
-    var starting_x = mid_point_x - (num / 2)
-    for i in num:
+  # TODO consider only children that are visible
+
+  if num_children % 2 == 0: # even num of children
+    var starting_x = mid_point_x - (num_children / 2)
+    for i in num_children:
       layout_child_packed(starting_x, i, controls[i])
   else: # odd num of children
     var width_unit = controls[0].rect_size.x
-    var starting_x = mid_point_x - floor(num / 2) * width_unit
-    for i in num:
+    var starting_x = mid_point_x - floor(num_children / 2) * width_unit
+    for i in num_children:
       layout_child_packed(starting_x, i, controls[i])
 
 
 func layout_children_spread() -> void:
   for i in get_child_count():
-      var child = get_child(i)
-      if child.is_class("Control"):
-        layout_child_spread(i, child)
+    var child = get_child(i)
+    if child.is_class("Control"):
+      layout_child_spread(i, child)
 
 
 # spread children horizontally
@@ -71,9 +72,9 @@ func layout_child_packed(starting_x: int, i: int, child: Control) -> void:
   var available_width = rect_size.x
   var available_height = rect_size.y
   var center = rect_size / 2
-  var child_size = Vector2(get_child(0).rect_size.x, 0)
+  var child_size = Vector2(get_child(0).rect_size.x, 0) # assuming same size for all children
   var child_x_offset = child_size.x / 2
-  var child_pos = Vector2(starting_x * (i + 1) - child_x_offset, 0)
+  var child_pos = Vector2(starting_x + child_size.x * i - child_x_offset, 0)
 
   print("x["+str(i)+"]="+str(child_pos.x))
 
@@ -105,4 +106,4 @@ func _unhandled_key_input(event: InputEventKey) -> void:
     $TextureRect2.visible = !$TextureRect2.visible
   elif event.scancode == KEY_J:
     rect_pivot_offset = Vector2(rect_pivot_offset.x + 10, rect_pivot_offset.y + 10)
-  get_tree().set_input_as_handled()
+    get_tree().set_input_as_handled()
