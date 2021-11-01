8668bde0bbabdd4d99cf5500204bf93b4e9b52f7
Rui Teixeira <rui@vazteixeira.org>
2021-11-01 09:03:09 +0000

    improve comment on managing the hboxcontainer pivot offst

--- scripts/ScoreHBoxContainer.gd
+++ scripts/ScoreHBoxContainer.gd
@@ -11,12 +11,14 @@ func _on_child_visibility_changed():
 
 
 # we get notified to sort our children whenever there's a change in them (i.e.
-# one comes visible/invisible). when that happens we want to update our pivot
-# offset so it's always coincided with the center of this container:
+# one becomes visible/invisible). when that happens we want to update our pivot
+# offset so it's always coinciding with the center of this hboxcontainer:
 #              score:  [ 1 2 3 ]    [ 1 2 ]  [ 1 ]
 # pivot offset H pos:      x           x       x
 # if we don't do this, the pivot offset stays the same regardless of how wide
-# this container is
+# this container is. this causes transform operations (like scaling) to not
+# look decent, as they will be done relative to a pivot point that is not the
+# correct one
 
 func _notification(what):
   if what == NOTIFICATION_SORT_CHILDREN:
